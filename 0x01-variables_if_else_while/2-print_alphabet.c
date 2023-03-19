c4f816bc35e34ed4b1cb030538bf39907f06d396
Ris254 <risperkarendi@gmail.com>
2023-03-19 09:26:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter 'a'; letter 'z'; letter++)
+		putcher(letter);
+	putcher('\n')
+	return (0)
+}
