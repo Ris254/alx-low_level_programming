1c086dd6cee40a394da1fd4fee519eb88856679c
Ris254 <risperkarendi@gmail.com>
2023-03-19 16:13:58 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,11 +12,11 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = '0'; x < '9'; x++
+	for (x = '0'; x < '9'; x++)
 	{
-	for (y = x + 1; y <= '9'; y++
+	for (y = x + 1; y <= '9'; y++)
 	{
-	for (z = y + 1; z <= '9'; z++
+	for (z = y + 1; z <= '9'; z++)
 	{
 	if ((y != x) != z)
 	{
