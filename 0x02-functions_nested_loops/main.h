d7376421ecfc9ba29c835d5f9570c2b61a4a1f4f
Ris254 <risperkarendi@gmail.com>
2023-03-22 11:09:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,18 @@
+#ifndef main_h
+#define main_h
+
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_*10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 print_last_digit(int);
 void jack_baver(void);
-void (times_table(void)
+void (times_table(void);
 int add(int, int);
 int print_to_98(int n);
 void print_times_table(int n);
+
+#endif
